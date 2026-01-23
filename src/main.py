95ffebb9f9df2e0fa11fc3bfee1dade11f2b66e5
Ratan <ratan.ajit.singh.2@gmail.com>
2026-01-23 13:22:41 +0100

    created a padding box that makes it easy for the user to move around the screen with ease

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 import cv2
 import mediapipe as mp
 import pyautogui
-
+import numpy as np
 
 def main():
 
     cam = cv2.VideoCapture(0)
-
     face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks = True)
-
     screen_w, screen_h = pyautogui.size()
+
+    padding = 215
+    pyautogui.FAILSAFE = False
 
     if not cam.isOpened():
         print("cam not open")
@@ -23,12 +24,14 @@ def main():
             break
 
         frame = cv2.flip(frame,1)
-
+        frame_h, frame_w, _ = frame.shape
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         output = face_mesh.process(rgb_frame)
-
         landmark_points = output.multi_face_landmarks
-        frame_h, frame_w, _ = frame.shape
+
+        #created an active box so that the movement is smooth
+        cv2.rectangle(frame, (padding, padding), (frame_w - padding, frame_h - padding), (255, 0, 0), 2)
+
 
         if landmark_points:
             landmarks = landmark_points[0].landmark
@@ -48,6 +51,17 @@ def main():
                     x = int(landmark.x * frame_w)
                     y = int(landmark.y * frame_h)
                     cv2.circle(frame, (x,y), 3, (0,255,0), -1)
+
+                #mouse control logic
+                if id == 473:
+                    x = int(landmark.x * frame_w)
+                    y = int(landmark.y * frame_h)
+
+                    #mapping camera coords to screen coords with padding for smoothness
+                    screen_x = np.interp(x, (padding, frame_w - padding), (0,screen_w))
+                    screen_y = np.interp(y, (padding, frame_h - padding), (0, screen_h))
+                    pyautogui.moveTo(screen_x, screen_y)
+
 
         cv2.imshow('eye controlled mouse', frame)
 
